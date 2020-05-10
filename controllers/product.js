1db6c6b359eebfdbe17965adebf06a56bf38da20
grudge17 <shresthpal17@gmail.com>
2020-05-11 00:26:06 +0530

    Product and category Api

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,26 @@ const Product=require('../models/product')
 const fs=require('fs')
 const {errorHandler}=require('../helpers/dbErrorHandler')
 
+
+exports.productById=(req,res,next,id)=>{
+    Product.findById(id).exec((err,product)=>{
+        if(err||!product){
+            return res.status(400).json({
+                error:"Product not Found!!"
+            }) 
+        }
+        req.product=product
+        next();
+    })
+}
+
+exports.read=(req,res)=>{
+    req.product.photo=undefined
+    return res.json(req.product)
+}
+
+
+
 exports.create=(req,res)=>{
     let form =new formidable.IncomingForm()
     form.keepExtensions=true//for image extensions
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,15 @@
 const express=require('express')
 const router=express.Router()
 
-const {create}=require('../controllers/product')
+const {create,productById,read}=require('../controllers/product')
 const {requireSignin,isAuth,isAdmin}=require('../controllers/auth')
 const {userById}=require('../controllers/user')
 
+router.get('/product/:productId',read)
 router.post("/product/create/:userId",requireSignin,isAuth,isAdmin,create)
 
 router.param("userId",userById)//whenever we get user id as an parameter we should find that id and display all the details
 //we do this to make particular user dashboard.
+router.param("productId",productById)
+
 module.exports=router;
